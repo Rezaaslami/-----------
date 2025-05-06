45cbee5dcb0de792bcbf4a6bcce7363fefd6270f
Reza Aslami <aslamireza132@gmail.com>
2025-05-06 04:34:31 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// Check out below repository for more information about this Typed.js 
+// (Github repo-->Readme.md-->Customization)
+
+// https://github.com/mattboldt/typed.js/
+
+var typed = new Typed(".text-type",{
+    strings: ["React Developer","Public Relations","Youtuber" ],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop : true
+})
